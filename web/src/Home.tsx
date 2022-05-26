cca2ef4f7d2998dd3eb0407f0749c7c9851404dd
Kevin Wang <kevmo314@gmail.com>
2022-05-26 18:51:51 -0400

    feat: add qr code

--- web/src/Home.tsx
+++ web/src/Home.tsx
@@ -2,14 +2,21 @@ import { Component, createMemo } from "solid-js";
 import { v4 as uuidv4 } from "uuid";
 
 const Home: Component = () => {
-  const uuid = createMemo(() => uuidv4());
-  return (
-    <div>
-      <div>RealtimeKit</div>
-      <div>Add a browser source in OBS: kit.rtirl.com/{uuid()}</div>
-      <div>Download a control app.</div>
-    </div>
-  );
+    const uuid = createMemo(() => uuidv4());
+    const url = createMemo(
+      () => encodeURIComponent(window.location.origin) + "/" + uuid()
+    );
+    return (
+      <div>
+        <div>RealtimeKit</div>
+        <div>Add a browser source in OBS: kit.rtirl.com/{uuid()}</div>
+        <img
+          src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url()}`}
+          alt="qr code"
+        />
+        <div>Download a control app.</div>
+      </div>
+    );
 };
 
 export default Home;
