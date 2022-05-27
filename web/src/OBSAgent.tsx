b5c2699b248a9090c2dc2b25225e386d5372ac2c
Kevin Wang <kevmo314@gmail.com>
2022-05-26 19:39:01 -0700

    fix: grammar

--- web/src/OBSAgent.tsx
+++ web/src/OBSAgent.tsx
@@ -656,7 +656,7 @@ const OBSAgent: Component<{ channelId: string }> = ({ channelId }) => {
                 d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
               />
             </svg>
-            This browser source doesn't have enough permissions. See
+            This browser source doesn't have enough permission. See
             kit.rtirl.com for instructions.
             <br />
             This warning will disappear in 60 seconds.
