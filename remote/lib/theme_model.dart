fbc95a08ae682521befe8ba73925907c1d20903a
varshini2402 <59878825+varshini2402@users.noreply.github.com>
2022-06-30 15:08:15 +0530

    Create theme_model.dart

---
+++ remote/lib/theme_model.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_final_fields
+
+import 'package:flutter/cupertino.dart';
+import 'theme_preferences.dart';
+
+class ThemeModel extends ChangeNotifier{ 
+  bool _isDark=false;
+  ThemePreferences _preferences=ThemePreferences();
+  bool get isDark => _isDark;
+
+
+getPreferences() async { 
+  _isDark =await _preferences.getTheme();
+  notifyListeners();
+
+}
+
+set isDark(bool value){ 
+  _isDark=value;
+  _preferences.setTheme(value);
+  notifyListeners();
+}
+
+}
