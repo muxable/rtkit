b5df4aeb5fd195ed156b658bf7a2a05748af1dfc
varshini2402 <59878825+varshini2402@users.noreply.github.com>
2022-06-30 17:56:33 +0530

    Update chat.dart

--- remote/lib/chat.dart
+++ remote/lib/chat.dart
@@ -1,7 +1,8 @@
 
-// ignore_for_file: camel_case_types, prefer_const_constructors
+// ignore_for_file: camel_case_types, prefer_const_constructors, unnecessary_const
 
 import 'package:flutter/material.dart';
+import 'package:remote/home.dart';
 
 class chat extends StatelessWidget {
   const chat({Key? key}) : super(key: key);
@@ -9,7 +10,38 @@ class chat extends StatelessWidget {
   @override
   Widget build(BuildContext context)
     => Scaffold(
-         appBar: AppBar(
+         appBar: AppBar(actions: [PopupMenuButton<int>(
+                color: Colors.indigo,
+                onSelected: (item) => onSelected(context, item),
+                itemBuilder: (context) => [
+                 
+                  
+                       const PopupMenuItem<int>(
+                        value:0,
+                        child:const Text('Settings'),
+                      ),
+                    const PopupMenuItem<int>(
+                        value:1,
+                        child:Text('Remove Ads'),
+                      ),
+                       const PopupMenuItem<int>(
+                        value:2,
+                        child:Text('Feature Requests'),
+                      ),
+                       const PopupMenuItem<int>(
+                        value:3,
+                        child:Text('Error Log'),
+                      ),
+                        const PopupMenuItem<int>(
+                        value:4,
+                        child:const Text('Licenses'),
+                      ),
+                        const PopupMenuItem<int>(
+                        value:5,
+                        child:const Text('About'),
+                      ),
+                ],
+              ),],
        
        title: Text("Twitch chat"),
       
