b8f36275654db1eed3f12fb5df5286d6e3ed2a57
varshini2402 <59878825+varshini2402@users.noreply.github.com>
2022-06-30 15:08:21 +0530

    Create theme_preferences.dart

---
+++ remote/lib/theme_preferences.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:shared_preferences/shared_preferences.dart';
+class ThemePreferences{ 
+  static const PREF_KEY ='pref_key';
+  setTheme(bool value)async{ 
+    SharedPreferences sharedPreferences=await SharedPreferences.getInstance();
+    sharedPreferences.setBool(PREF_KEY, value);
+  }
+  getTheme()async{ 
+     SharedPreferences sharedPreferences=await SharedPreferences.getInstance();
+    sharedPreferences.getBool(PREF_KEY);
+  }
+}
