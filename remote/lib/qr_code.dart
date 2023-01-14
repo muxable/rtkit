393a52e028c46cd74bafe674d6d82ed678e0ff0e
Juan <juaoose@users.noreply.github.com>
2023-01-13 21:51:20 -0500

    fix: migrate to mobile scanner and update validation logic

--- remote/lib/qr_code.dart
+++ remote/lib/qr_code.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:remote/operations.dart';
 import 'package:remote/storage_util.dart';
 import 'package:remote/variables.dart';
@@ -17,20 +13,8 @@ class Qrcode extends StatefulWidget {
 
 class _QrcodeState extends State<Qrcode> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  Barcode? result;
-  QRViewController? controller;
-
-  // In order to get hot reload to work we need to pause the camera if the platform
-  // is android, or resume the camera if the platform is iOS.
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    } else if (Platform.isIOS) {
-      controller!.resumeCamera();
-    }
-  }
+  final urlRegEx = RegExp(
+      r'https?:\/\/?kit.rtirl.com\/(?:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})');
 
   @override
   Widget build(BuildContext context) {
@@ -41,77 +25,32 @@ class _QrcodeState extends State<Qrcode> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 5,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: _onQRViewCreated,
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: (result != null)
-                  ? Text(
-                      'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}',
-                      textAlign: TextAlign.center,
-                    )
-                  // ignore: prefer_const_constructors
-                  : Text('Scan a code'),
-            ),
-          )
-        ],
+      body: Expanded(
+        child: MobileScanner(
+            allowDuplicates: false,
+            onDetect: (barcode, args) async {
+              final code = barcode.rawValue;
+              if (code != null) {
+                if (!urlRegEx.hasMatch(code)) {
+                  _showErrorSnackBar();
+                }
+
+                final splitUrl = code.split('/');
+                uuid = splitUrl[3];
+                StorageUtil.putString("uuid", uuid!);
+
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const Operations()));
+              }
+              Navigator.of(context).pop();
+            }),
       ),
     );
   }
 
-  Future<void> _onQRViewCreated(QRViewController controller) async {
-    this.controller = controller;
-
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        controller.pauseCamera();
-        result = scanData;
-        qr1(result!);
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
-
-  Future<void> qr1(Barcode result) async {
-    data = result.code.toString();
-    var arr = data!.split('/');
-    uuid = arr[3];
-    StorageUtil.putString("uuid", uuid!);
-    fetchuuid();
-  }
-
-  fetchuuid() async {
-    String s = "https://kit.rtirl.com/api/$uuid/activeAgentId";
-
-    final response = await http.get(Uri.parse(s));
-
-    if (response.statusCode == 200) {
-      navigate();
-
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Invalid uuid');
-    }
-  }
-
-  void navigate() {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => const Operations()));
+  void _showErrorSnackBar() {
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('This QR code is not valid, please check again!'),
+    ));
   }
 }
