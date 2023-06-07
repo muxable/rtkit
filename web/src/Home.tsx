9a445499be37971de6cdd2176db7c2b1d1656d73
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2023-06-07 14:24:59 -0700

    fix: revamp page (#95)
    * fix: revamp page
    
    * fix: audit
    
    * fix: kit subdomain
    
    * fix: test render for mobile
    
    * fix: revert
    
    * fix: link redirects
    
    * fix: clearer instructions
    
    * fix: remove redundant div
    
    * fix: remove redundant text

--- web/src/Home.tsx
+++ web/src/Home.tsx
@@ -10,33 +10,39 @@ const Home: Component = () => {
   );
   return (
     <div>
-      <div>RealtimeKit</div>
-      <div>Add a browser source in OBS: https://kit.rtirl.com/{uuid()}</div>
+      <h1>RealtimeKit</h1>
+      <h2>Control OBS remotely without setting up obs-websocket</h2>
+      <div>Add a browser source in OBS: <Link target="_blank" href={`/${uuid()}`}>https://kit.rtirl.com/{uuid()}</Link></div>
       <img
         src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url()}`}
         alt="qr code"
       />
       <div>Make sure it's configured like this:</div>
       <img src={obsSettingsImg} alt="configuration" />
-      <div>Download a control app.</div>
+      <div>Download a control app</div>
       <Link
-        href="https://play.google.com/store/apps/details?id=com.rtirl.kit"
-        target="_blank"
-      >
-        Android
-      </Link>
+        href="https://play.google.com/store/apps/details?id=com.rtirl.chat&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+        target="_blank"><img
+        id="google-play"
+        alt="Get it on Google Play"
+        src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+        width="300"
+      /></Link>
       <br />
       <Link
-        href="https://apps.apple.com/us/app/realtimekit/id6443628121"
-        target="_blank"
-      >
-        iOS
-      </Link>
+        href="https://apps.apple.com/app/id1567720948"
+        target="_blank"><img
+        id="app-store"
+        alt="Download on the app store"
+        src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+        width="245"
+      /></Link>
       <br />
-      <Link href={`/${uuid()}`}>Web Interface</Link>
+      <div>Scan the QR code in the app and enjoy</div>
+      <div>The app might have some delays for best reliability use the <Link href={`/${uuid()}`} target="_blank">Web Interface</Link> and save the link for later</div>
       <div>Make a new app with the API</div>
       <div>
-        Ask on <a href="https://discord.gg/UKHJMQs74u">discord</a> ,
+        Ask on <a href="https://discord.gg/UKHJMQs74u">discord</a>,
         documentation isn't done yet.
       </div>
     </div>
