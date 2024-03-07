4accdbd1439490a05de044ffd210a216ab7660b6
Bill Odida <billodida420@gmail.com>
2024-03-06 20:32:46 -0800

    Adding torch to scanner

--- remote/lib/screens/qr_scanner.dart
+++ remote/lib/screens/qr_scanner.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:provider/provider.dart';
 import 'package:rtkit/components/channel_provider.dart';
+import 'package:rtkit/components/scanner_settings.dart';
 import 'package:rtkit/screens/control_screen.dart';
 
 class QRScanner extends StatelessWidget {
@@ -12,6 +13,12 @@ class QRScanner extends StatelessWidget {
     final urlRegEx = RegExp(
         r'https?:\/\/?kit.rtirl.com\/(?:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})');
 
+    MobileScannerController _scanController = MobileScannerController(
+      // facing: CameraFacing.back,
+      // torchEnabled: false,
+      detectionSpeed: DetectionSpeed.noDuplicates,
+    );
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -19,28 +26,44 @@ class QRScanner extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: MobileScanner(onDetect: (BarcodeCapture barcode) {
-        final code = barcode.raw;
-        if (code != null) {
-          if (!urlRegEx.hasMatch(code)) {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text('This QR code is not valid, please check again!'),
-            ));
-            return;
-          }
+      body: Stack(
+        children: [
+          MobileScanner(
+              controller: _scanController,
+              onDetect: (BarcodeCapture barcode) {
+                final code = barcode.raw;
+                if (code != null) {
+                  if (!urlRegEx.hasMatch(code)) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text(
+                          'This QR code is not valid, please check again!'),
+                    ));
+                    return;
+                  }
 
-          final splitUrl = code.split('/');
-          final channelId = splitUrl[3];
+                  final splitUrl = code.split('/');
+                  final channelId = splitUrl[3];
 
-          Provider.of<ChannelModel>(context, listen: false)
-              .setChannelId(channelId);
+                  Provider.of<ChannelModel>(context, listen: false)
+                      .setChannelId(channelId);
 
-          Navigator.of(context).pushReplacement(MaterialPageRoute(
-              builder: (context) => ControlScreen(channelId: channelId)));
-          return;
-        }
-        Navigator.of(context).pop();
-      }),
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) =>
+                          ControlScreen(channelId: channelId)));
+                  return;
+                }
+                Navigator.of(context).pop();
+
+                
+              }),
+          Positioned(
+            top: 50,
+            left: 0,
+            right: 0,
+            child: ScannerSettings(scanController: _scanController),
+          ),
+        ],
+      ),
     );
   }
 }
