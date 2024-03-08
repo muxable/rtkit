c8f1c7388ceea0c81a40abb11d1ddd0fde7af3dc
Bill Odida <billodida420@gmail.com>
2024-03-07 23:24:00 -0800

    fixing torch state

--- remote/lib/screens/qr_scanner.dart
+++ remote/lib/screens/qr_scanner.dart
@@ -2,34 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:provider/provider.dart';
 import 'package:rtkit/components/channel_provider.dart';
-import 'package:rtkit/components/scanner_settings.dart';
 import 'package:rtkit/screens/control_screen.dart';
 
-class QRScanner extends StatelessWidget {
+class QRScanner extends StatefulWidget {
   const QRScanner({Key? key}) : super(key: key);
+
+  @override
+  State<QRScanner> createState() => _QRScannerState();
+}
+
+class _QRScannerState extends State<QRScanner> {
+
+   MobileScannerController scanController = MobileScannerController(
+      // facing: CameraFacing.back,
+      // torchEnabled: false,
+      detectionSpeed: DetectionSpeed.noDuplicates,
+    );
+
+  TorchState value = TorchState.off;
 
   @override
   Widget build(BuildContext context) {
     final urlRegEx = RegExp(
         r'https?:\/\/?kit.rtirl.com\/(?:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})');
 
-    MobileScannerController _scanController = MobileScannerController(
-      // facing: CameraFacing.back,
-      // torchEnabled: false,
-      detectionSpeed: DetectionSpeed.noDuplicates,
-    );
+   
 
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         leading: const BackButton(),
+        actions : [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  value = value == TorchState.on ? TorchState.off : TorchState.on;
+                   scanController.toggleTorch();
+                });
+               
+              } ,
+              icon: Icon(
+                value == TorchState.on ? Icons.flash_off : Icons.flash_on,
+              ),
+            ),
+        ],
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
       body: Stack(
         children: [
           MobileScanner(
-              controller: _scanController,
+              controller: scanController,
               onDetect: (BarcodeCapture barcode) {
                 final code = barcode.raw;
                 if (code != null) {
@@ -54,12 +77,7 @@ class QRScanner extends StatelessWidget {
                 }
                 Navigator.of(context).pop();
               }),
-          Positioned(
-            top: 50,
-            left: 0,
-            right: 0,
-            child: ScannerSettings(scanController: _scanController),
-          ),
+         
         ],
       ),
     );
